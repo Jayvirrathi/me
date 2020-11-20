5b8b1723fb8f1949c7040e5c78892a8038a4c4d6
Jayvir Rathi <jayvirrathi@gmail.com>
2020-11-20 12:06:02 +0530

    added job timeline

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,106 +1,156 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class Timeline extends Component {
-  render() {
-    return (
-      <div>
-        <section className="colorlib-experience" data-section="timeline">
-          <div className="colorlib-narrow-content">
-            <div className="row">
-              <div
-                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
-                data-animate-effect="fadeInLeft"
-              >
-                <span className="heading-meta">highlights</span>
-                <h2 className="colorlib-heading animate-box">Timeline</h2>
-              </div>
+    render() {
+        return (
+            <div>
+                <section
+                    className='colorlib-experience'
+                    data-section='timeline'
+                >
+                    <div className='colorlib-narrow-content'>
+                        <div className='row'>
+                            <div
+                                className='col-md-6 col-md-offset-3 col-md-pull-3 animate-box'
+                                data-animate-effect='fadeInLeft'
+                            >
+                                <span className='heading-meta'>highlights</span>
+                                <h2 className='colorlib-heading animate-box'>
+                                    Timeline
+                                </h2>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-md-12'>
+                                <div className='timeline-centered'>
+                                    <article
+                                        className='timeline-entry animate-box'
+                                        data-animate-effect='fadeInLeft'
+                                    >
+                                        <div className='timeline-entry-inner'>
+                                            <div className='timeline-icon color-5'>
+                                                <i className='icon-pen2' />
+                                            </div>
+                                            <div className='timeline-label'>
+                                                <h2>
+                                                    Job at Innovify{' '}
+                                                    <span>July-Oct 2020</span>
+                                                </h2>
+                                                <p>
+                                                    I worked for 4 months at
+                                                    Innovify as a Jr.Software
+                                                    Developer in the Development
+                                                    team. My major part of the
+                                                    work has been full stack
+                                                    development. I am also
+                                                    learning CI/CD, docker &
+                                                    kubernetes.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article
+                                        className='timeline-entry animate-box'
+                                        data-animate-effect='fadeInLeft'
+                                    >
+                                        <div className='timeline-entry-inner'>
+                                            <div className='timeline-icon color-3'>
+                                                <i className='icon-pen2' />
+                                            </div>
+                                            <div className='timeline-label'>
+                                                <h2>
+                                                    Internship at Innovify{' '}
+                                                    <span>Jan-Jun 2020</span>
+                                                </h2>
+                                                <p>
+                                                    I did my internship at
+                                                    Innovify as a Software
+                                                    Developer-Intern in the
+                                                    Development team. My major
+                                                    part of the work has been
+                                                    full stack development. I am
+                                                    also exploring the role of
+                                                    backend technology like
+                                                    nodejs & frontend Technology
+                                                    like reactjs.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article
+                                        className='timeline-entry animate-box'
+                                        data-animate-effect='fadeInTop'
+                                    >
+                                        <div className='timeline-entry-inner'>
+                                            <div className='timeline-icon color-4'>
+                                                <i className='icon-pen2' />
+                                            </div>
+                                            <div className='timeline-label'>
+                                                <h2>
+                                                    Master at DAIICT{' '}
+                                                    <span>2018-2020</span>
+                                                </h2>
+                                                <p>
+                                                    I am pursuing my
+                                                    post-graduation studies with
+                                                    major in I.T.(Information
+                                                    Technology). I have taken
+                                                    courses like DSA, OOPs,
+                                                    Computer Networks,Database
+                                                    Management over the years
+                                                    and have better
+                                                    understanding of these
+                                                    subjects.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article
+                                        className='timeline-entry animate-box'
+                                        data-animate-effect='fadeInLeft'
+                                    >
+                                        <div className='timeline-entry-inner'>
+                                            <div className='timeline-icon color-5'>
+                                                <i className='icon-pen2' />
+                                            </div>
+                                            <div className='timeline-label'>
+                                                <h2>
+                                                    Bachelor at GLS{' '}
+                                                    <span>2015-2018</span>
+                                                </h2>
+                                                <p>
+                                                    I have completed my
+                                                    bachelors education with
+                                                    basic subjects as DSA, OOPs,
+                                                    Computer Networks,Database
+                                                    Management. During my time
+                                                    at college, I have developed
+                                                    interest in solving complex
+                                                    problems using technologies
+                                                    with the fundamental
+                                                    understanding of problem as
+                                                    per need appropriate data
+                                                    structures & technologies
+                                                    skills used to actually
+                                                    solve the problems.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article
+                                        className='timeline-entry begin animate-box'
+                                        data-animate-effect='fadeInBottom'
+                                    >
+                                        <div className='timeline-entry-inner'>
+                                            <div className='timeline-icon color-none'></div>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-            <div className="row">
-              <div className="col-md-12">
-                <div className="timeline-centered">
-                  <article
-                    className="timeline-entry animate-box"
-                    data-animate-effect="fadeInLeft"
-                  >
-                    <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-3">
-                        <i className="icon-pen2" />
-                      </div>
-                      <div className="timeline-label">
-                        <h2>
-                          Internship at Innovify <span>2020-present</span>
-                        </h2>
-                        <p>
-                          I recently have joined the Innovify as a Software
-                          Developer-Intern in the Development team. My major
-                          part of the work has been full stack development. I am
-                          also exploring the role of backend technology like
-                          nodejs & frontend Technology like reactjs.
-                        </p>
-                      </div>
-                    </div>
-                  </article>
-                  <article
-                    className="timeline-entry animate-box"
-                    data-animate-effect="fadeInTop"
-                  >
-                    <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-4">
-                        <i className="icon-pen2" />
-                      </div>
-                      <div className="timeline-label">
-                        <h2>
-                          Master at DAIICT<span>2018-2020</span>
-                        </h2>
-                        <p>
-                          I am pursuing my post-graduation studies with major in
-                          I.T.(Information Technology). I have taken courses
-                          like DSA, OOPs, Computer Networks,Database Management
-                          over the years and have better understanding of these
-                          subjects.
-                        </p>
-                      </div>
-                    </div>
-                  </article>
-                  <article
-                    className="timeline-entry animate-box"
-                    data-animate-effect="fadeInLeft"
-                  >
-                    <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-5">
-                        <i className="icon-pen2" />
-                      </div>
-                      <div className="timeline-label">
-                        <h2>
-                          Bachelor at GLS <span>2015-2018</span>
-                        </h2>
-                        <p>
-                          I have completed my bachelors education with basic
-                          subjects as DSA, OOPs, Computer Networks,Database
-                          Management. During my time at college, I have
-                          developed interest in solving complex problems using
-                          technologies with the fundamental understanding of
-                          problem as per need appropriate data structures &
-                          technologies skills used to actually solve the
-                          problems.
-                        </p>
-                      </div>
-                    </div>
-                  </article>
-                  <article
-                    className="timeline-entry begin animate-box"
-                    data-animate-effect="fadeInBottom"
-                  >
-                    <div className="timeline-entry-inner">
-                      <div className="timeline-icon color-none"></div>
-                    </div>
-                  </article>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    );
-  }
+        );
+    }
 }
